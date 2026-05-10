2a6f6cdc4fbb7294eb0b2c402d29f4431e0e3e89
Abhishek Bhatia <abhibhatia0410@gmail.com>
2026-05-10 21:01:24 +0530

    backend changed

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.post(
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     methods: ["POST", "GET", "PUT", "DELETE", "PATCH", "OPTIONS"],
     credentials: true,
   })
